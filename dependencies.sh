163e982ddd50cc0b64cf52cbea1a22c2458707af
ike709 <ike709@users.noreply.github.com>
2021-07-24 16:03:33 -0400

    Bump auxmos to 0.2.3, fixes fusion (#4927)
    Co-authored-by: ike709 <ike709@beestation13.com>

--- dependencies.sh
+++ dependencies.sh
@@ -21,3 +21,9 @@ export NODE_VERSION_PRECISE=12.20.0
 
 # SpacemanDMM git tag
 export SPACEMAN_DMM_VERSION=suite-1.7
+<<<<<<< HEAD
+=======
+
+#auxmos version
+export AUXMOS_VERSION=0.2.3
+>>>>>>> 20ed0e9732... Bump auxmos to 0.2.3, fixes fusion (#4927)
